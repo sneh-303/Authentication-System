ece84a08bd7638db0e2d69560641bf575228bee1
sneh-303 <snehshial@gmail.com>
2025-06-23 18:12:38 +0530

    Updated code

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   baseURL: `https://authentication-system-3-y5zg.onrender.com/api`,
+  withCredentials: true, 
   // baseURL: `https://authentication-system-3-y5zg.onrender.com/api`,
 });
 
@@ -14,7 +15,8 @@ const getRequest = (endpoint) => {
   return api.get(endpoint, {
     headers: {
       Authorization: token ? `Bearer ${token}` : ''
-    }
+    },
+    withCredentials: true, 
   });
 };
 
